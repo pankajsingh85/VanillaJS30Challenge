b54ee7295c7220a91881d8cb7e5419bc0f3e7a08
pankaj singh <pankajsingh8540@gmail.com>
2021-02-14 16:14:36 +0530

    change some css

--- Day2-CSS+JS clock/script.js
+++ Day2-CSS+JS clock/script.js
@@ -5,22 +5,18 @@ const hands = document.querySelectorAll('.hand');
 const hour = document.querySelector('.hourT');
 const time = document.querySelector('.time');
 
-
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    // console.log(seconds);
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 
 
     const mins = now.getMinutes();
-    // console.log(mins);
     const minDegrees = ((mins / 60) * 360) + 90;
     minHand.style.transform = `rotate(${minDegrees}deg)`;
 
     const hours = now.getHours();
-    // console.log(hours);
     const hourDegrees = ((hours / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
@@ -30,7 +26,7 @@ function setDate() {
         hands.forEach(hand => hand.style.transitionDuration = '0.05s');
     }
     hour.innerHTML = hours;
-    time.innerHTML = ":" + mins + ":" + seconds;
+    time.textContent = ":" + mins + ":" + seconds;
 }
 // const hands 
 setInterval(setDate, 1000);
